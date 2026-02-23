be91c4fd28a8566e6f356ac22ae0ad6ed4b149ce
ol9999 <owenstuartlee@gmail.com>
2026-02-23 18:26:08 -0500

    Get the usernames we need to scrape

--- scraper/users.py
+++ scraper/users.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import os
 import jsonlines
 
-def scrape_users():
+def get_usernames():
     
     # The number for the intermediate files we will be using
     x = sys.argv[1]
@@ -18,6 +18,32 @@ def scrape_users():
             for line in reader:
                 visited.add(line[0])
 
-    print(visited)
+    # Add missing users to visited set
+    missing_users_path = str(Path(__file__).parent.parent / "data" / f"missing_users_{x}.txt")
+    if os.path.exists(missing_users_path):
+        with open(missing_users_path) as reader:
+            for line in reader:
+                visited.add(line.strip())
+
+    # The set of users to visit
+    usernames = set()
+
+    # Add usernames to usernames set
+    usernames_path = str(Path(__file__).parent.parent / "data" / f"usernames_{x}.txt")
+    if os.path.exists(usernames_path):
+        with open(usernames_path) as reader:
+            for line in reader:
+                usernames.add(line.strip())
+    
+    # Remove usernames we have already visited
+    usernames -= visited
+
+    return usernames
+
+def scrape_users():
+    
+    usernames = get_usernames()
+
+    print(usernames)
 
 scrape_users()
